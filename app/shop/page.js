8a4cc5fa257b116e44861953c700a70c21571d3b
pratikdossiefoyer <pratik.mane@dossiefoyer.in>
2025-02-02 19:09:21 +0530

    memo technique in product

--- app/shop/page.js
+++ app/shop/page.js
@@ -1,12 +1,22 @@
+// page.tsx
 "use client";
-import React from "react";
-import ShopContent from "./compoenent/ShopContent";
+
+import React, { Suspense } from "react";
+import ShopContent from "./component/ShopContent";
+
+function LoadingFallback() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-sky-600"></div>
+    </div>
+  );
+}
 
 function Page() {
   return (
-    <div className="container mx-auto px-4">
+    <Suspense fallback={<LoadingFallback />}>
       <ShopContent />
-    </div>
+    </Suspense>
   );
 }
 
